23451b6b589f5adfa49b2ef6fff68b78c6a68129
kawasaki0201 <165142552+kawasaki0201@users.noreply.github.com>
2024-06-14 12:39:50 +0900

    20240614

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from typing import Optional
 
 from fastapi import FastAPI
+
 import random
+
+from fastapi.responses import HTMLResponse #インポート
+
 app = FastAPI()
 
 
@@ -29,3 +33,21 @@ def omikuji():
     ]
 
     return omikuji_list[random.randrange(10)]
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+@app.post("/present")
+async def new_naming(present):
+    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}
